5ba9e40a0386b96f2db6988df4183cd3740f96fb
SOLODILLYA <31259111+SOLODILLYA@users.noreply.github.com>
2024-05-06 19:49:57 +0200

    Upload of work

---
+++ SocketsDemo/Client/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - Create parameters of connection
+            int port = 9001;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ep = new IPEndPoint(ip, port);
+
+            //2 - Create a cycle to send messages to server
+            try
+            {
+                string ans = "y";
+                while(ans ==  "y")
+                {
+                    //2.1 Dialogue to enter message
+                    Console.Write("\n> Enter message: ");
+                    string message = Console.ReadLine();
+
+                    //2.2 Create client current socket
+                    Socket sender = new Socket(
+                        AddressFamily.InterNetwork,
+                        SocketType.Stream,
+                        ProtocolType.IP
+                    );
+
+                    //2.3 Connect to server
+                    sender.Connect(ep);
+
+                    //2.4 Encode and send message to server
+                    byte[] buff1 = Encoding.UTF8.GetBytes(message);
+                    sender.Send(buff1);
+
+                    //2.5 Recieve response from server
+                    byte[] buff2 = new byte[1024];
+                    int bytes = sender.Receive(buff2);
+
+                    //2.6 Decode and print response from server
+                    string response = Encoding.UTF8.GetString(buff2, 0 ,bytes);
+                    Console.WriteLine($"    Response from server: {response}");
+
+                    //2.7 Close connection with server
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                    //2.8 Check for force stop
+                    if (message == "STOP-123")
+                        break;
+
+                    //2.9 Check for continuation
+                    Console.Write("\n More messages (y/n) - ");
+                    ans = Console.ReadLine();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Client error: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("> Client was stopped");
+            }
+        }
+    }
+}
---
+++ SocketsDemo/Server/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - Create parameters of connection
+            int port = 9001;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ep = new IPEndPoint(ip, port);
+
+            //2 - Create server current socket
+            Socket listener = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.IP
+            );
+
+            //3 - Start server socket
+            int queueLength = 10;
+            listener.Bind(ep);
+            listener.Listen(queueLength);
+
+            //4 - Start cycle to work with clients
+            try 
+            { 
+                while(true)
+                {
+                    //4.1. Waiting, connection and creation of accepting socket
+                    Console.WriteLine("\n> Waiting for connection attempts");
+
+                    Socket acceptor = listener.Accept();
+
+                    //4.2 Recieving of input message from client
+                    byte[] buff = new byte[1024];
+                    int bytes = acceptor.Receive(buff);
+
+                    //4.3 Decoding of recieved message
+                    string message = Encoding.UTF8.GetString(buff, 0, bytes);
+                    Console.WriteLine($"    {DateTime.Now} -> {message}");
+
+                    //4.4 Parsing of recieved message
+                    string response = "This is response of server";
+
+                    //4.5 Encoding and sending of response to client
+                    byte[] buff2 = new byte[1024];
+                    buff2 = Encoding.UTF8.GetBytes(response);
+                    acceptor.Send(buff2);
+
+                    //4.6 End of connection with client
+                    acceptor.Shutdown(SocketShutdown.Both);
+                    acceptor.Close();
+
+                    //4.7 Check for force stop
+                    if (message == "STOP-123")
+                        break;
+                }
+                listener.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Server error: {ex.Message}");
+            }
+            finally
+            {
+                listener?.Close();
+                Console.WriteLine("> Client was stopped");
+            }
+        }
+    }
+}
