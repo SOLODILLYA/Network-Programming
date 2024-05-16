c27c2d0cb466f674c36600f410d96b0362d88cd9
SOLODILLYA <31259111+SOLODILLYA@users.noreply.github.com>
2024-05-16 16:18:47 +0200

    MultiChat Finished

--- MultiChat/MultiChat/MainWindow.xaml.cs
+++ MultiChat/MultiChat/MainWindow.xaml.cs
@@ -31,12 +31,78 @@ namespace MultiChat
 
         private void MulticastSend(string message)
         {
+            // 1 - Creation of UDP Socket to send messages 
+            Socket socket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp
+            );
 
+            // 2 - Set time to live of datagram
+            socket.SetSocketOption(
+                SocketOptionLevel.IP,
+                SocketOptionName.MulticastTimeToLive,
+                2
+            );
+
+            // 3 - Registration of special adress of mass send
+            IPAddress addr = IPAddress.Parse("224.5.5.5");
+            socket.SetSocketOption(
+                SocketOptionLevel.IP,
+                SocketOptionName.AddMembership,
+                new MulticastOption(addr)
+            );
+
+            // 4 - Create endpoint and connect to service
+            IPEndPoint ep = new IPEndPoint(addr, 9001);
+            socket.Connect(ep);
+
+            // 5 - Send Message
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            socket.Send(data);
+
+            // 6 - Close sending socket
+            socket.Close();
         }
 
         private void MulticastReceive()
         {
+            while (true)
+            {
+                // 1 - Creation of UDP Socket to receive messages 
+                Socket socket = new Socket(
+                    AddressFamily.InterNetwork,
+                    SocketType.Dgram,
+                    ProtocolType.Udp
+                );
 
+                // 2 - Create Endpoint
+                IPEndPoint ep = new IPEndPoint(IPAddress.Any, 9001);
+                socket.Bind(ep);
+
+                // 3 - Sign method recieve to receive messages
+                IPAddress addr = IPAddress.Parse("224.5.5.5");
+                socket.SetSocketOption(
+                    SocketOptionLevel.IP,
+                    SocketOptionName.AddMembership,
+                    new MulticastOption(addr, IPAddress.Any)
+                );
+
+                // 4 - Receive message
+                byte[] buff = new byte[1024];
+                socket.Receive(buff);
+                string mess = Encoding.UTF8.GetString(buff);
+
+                //5 Show message
+                Dispatcher.Invoke(new Action(() =>
+                {
+
+                    ChatBox.Text += mess;
+                }));
+
+                // 5 - Close socket
+                socket.Close();
+            }
         }
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
@@ -61,6 +127,13 @@ namespace MultiChat
                 MessageField.Clear();
                 MessageField.Focus();
             }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Thread listenThread = new Thread(new ThreadStart(MulticastReceive));
+            listenThread.IsBackground = true;
+            listenThread.Start();
         }
     }
 }
